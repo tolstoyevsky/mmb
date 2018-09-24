98d1c510d67ac251f1dad3985ceaf7fcacf5b96f
PopovaPolina <ppopova.wis@gmail.com>
2018-09-24 16:42:39 +0300

    Run node main.js with retries

--- rocketchat/docker-entrypoint.sh
+++ rocketchat/docker-entrypoint.sh
@@ -8,5 +8,10 @@ export MONGO_URL="mongodb://${MONGO_HOST}/${MONGO_DATABASE}"
 
 wait-for-it.sh -h "${host}" -p "${port}" -t 90 -- >&2 echo "MongoDB is ready"
 
-node main.js
-
+n=0
+until [ ${n} -ge 5 ]; do
+  node main.js && break
+  n=$[$n + 1]
+  info "retrying in 1 sec"
+  sleep 1
+done
