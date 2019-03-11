ee674ff95095f420b33abf69f79883bdf02051dd
BehindLoader <simonsuprun@gmail.com>
2019-03-11 16:36:23 +0300

    rocketchat: there is a problem in docker-entrypoint.sh

--- rocketchat/docker-entrypoint.sh
+++ rocketchat/docker-entrypoint.sh
@@ -11,7 +11,7 @@ wait-for-it.sh -h "${host}" -p "${port}" -t 90 -- >&2 echo "MongoDB is ready"
 n=0
 until [ ${n} -ge 5 ]; do
   node main.js && break
-  n=$[$n + 1]
-  info "retrying in 1 sec"
+  n=$((n+1))
+  >&2 echo "retrying in 1 sec"
   sleep 1
 done
