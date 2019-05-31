f81f81a2a2bbb0ea77982090e8e29a6eff9d8833
BehindLoader <simonsuprun@gmail.com>
2019-05-31 11:40:07 +0300

    hubot-rocketchat: fix beer quotes in simpsons script

--- hubot-rocketchat/scripts/the-simpsons-quotes.js
+++ hubot-rocketchat/scripts/the-simpsons-quotes.js
@@ -16,9 +16,9 @@ module.exports = robot => {
   robot.hear(/beer/i, msg => {
     const beerQuotes = [
       "Beer... Now there's a temporary solution.",
-      "All right, brain.",
-      "You don't like me and I don't like you, but let's just do this and I can get back to killing you with beer.",
-      "Mmmm... beer", "Beer: The cause of, and solution to, all of life's problems.", 
+      "All right, brain. You don't like me and I don't like you, but let's just do this and I can get back to killing you with beer.",
+      "Mmmm... beer",
+      "Beer: The cause of, and solution to, all of life's problems.", 
       "Beer, beer, beer, bed, bed, bed.",
       "I would kill everyone in this room for a drop of sweet beer."
     ]
