37c743d25227526c5b85f21ae7db6870a9136df2
Evgeny Golyshev <eugulixes@gmail.com>
2020-04-29 23:19:04 +0300

    transmission-web: configure transmission via .env

--- transmission-web/docker-entrypoint.sh
+++ transmission-web/docker-entrypoint.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+ALLOWED=${ALLOWED:="192.168.*.*"}
+
+AUTH=${AUTH:=true}
+
+LOGIN=${LOGIN:="cusdeb"}
+
+PASSWORD=${PASSWORD:="cusdeb"}
+
+PORT=${PORT:=8003}
+
 if ${AUTH}; then
     transmission-daemon --auth --foreground --username ${LOGIN} --password ${PASSWORD} --port ${PORT} --allowed "${ALLOWED}"
 else
