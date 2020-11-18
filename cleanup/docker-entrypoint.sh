f3d7acc8de05a429f76aa2bfeeb24875be6a5829
Lera <dmit.valerya@yandex.ru>
2020-11-18 17:52:41 +0300

    Add cleanup

---
+++ cleanup/docker-entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+CRON_SCHEDULE=${CRON_SCHEDULE:=0\ 12\ *\ *\ 6}
+
+sed -i -e "s/CRON_SCHEDULE/${CRON_SCHEDULE}/" /var/spool/cron/crontabs/root
+
+>&2 echo "Start cleanup container"
+
+crond -L /var/log/cron.log
+
+>&2 echo "Start writing logs"
+while true; do
+    cat /var/log/cron.log & wait $!
+done
