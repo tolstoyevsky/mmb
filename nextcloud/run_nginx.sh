eebf65d5da13afcc8e43321d1072799b3b3dccfa
Evgeny Golyshev <eugulixes@gmail.com>
2021-05-31 13:03:28 +0300

    nextcloud: remove previous dockerization

--- nextcloud/run_nginx.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-sed -i -e "s/{DB_HOST}/${DB_HOST}/" /etc/php/7.0/fpm/pool.d/www.conf
-sed -i -e "s/{DB_NAME}/${DB_NAME}/" /etc/php/7.0/fpm/pool.d/www.conf
-sed -i -e "s/{DB_USERNAME}/${DB_USERNAME}/" /etc/php/7.0/fpm/pool.d/www.conf
-sed -i -e "s/{DB_PASSWORD}/${DB_PASSWORD}/" /etc/php/7.0/fpm/pool.d/www.conf
-
-sed -i -e "s/PORT/${PORT}/" /etc/nginx/sites-available/default
-nginx -g 'daemon off;'
