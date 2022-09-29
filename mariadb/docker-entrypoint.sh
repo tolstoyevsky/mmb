13fd5c65e354960e2b34b68b1895b7fd77e7c9d2
Evgeny Golyshev <eugulixes@gmail.com>
2022-09-29 18:59:14 +0300

    mariadb: use $(..) instead of legacy `..`

--- mariadb/docker-entrypoint.sh
+++ mariadb/docker-entrypoint.sh
@@ -35,7 +35,7 @@ else
     mysql_install_db --user=mysql --datadir=/var/lib/mysql
 
     if [[ "$MYSQL_ROOT_PASSWORD" = "" ]]; then
-        MYSQL_ROOT_PASSWORD=`pwgen 16 1`
+        MYSQL_ROOT_PASSWORD=$(pwgen 16 1)
         >&2 echo "[i] MySQL root Password: $MYSQL_ROOT_PASSWORD"
     fi
 
@@ -43,7 +43,7 @@ else
     MYSQL_USER=${MYSQL_USER:-""}
     MYSQL_PASSWORD=${MYSQL_PASSWORD:-""}
 
-    tfile=`mktemp`
+    tfile=$(mktemp)
     if [[ ! -f "${tfile}" ]]; then
         return 1
     fi
